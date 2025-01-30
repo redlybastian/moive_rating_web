44a3a25316a95c799dbb1b86c5572cfc37c1139f
redlybastian <red233786@gmail.com>
2025-01-30 14:29:59 +0530

    api get method set up

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function search() {
   const userInput = document.getElementById("search_input");
   const movieName = userInput.value;
-  const url = "http://www.omdbapi.com/?i=tt3896198&apikey=235792ba" + movieName;
+  const url = "" + movieName;
   const httpRequest = new XMLHttpRequest();
 
   httpRequest.open("GET", url);
@@ -12,7 +12,17 @@ function search() {
     console.log(httpRequest.response);
     const posterImage = document.getElementById("moviePoster");
     posterImage.src = httpRequest.response.Poster;
+
     const movieTitle = document.getElementById("movie_title");
     movieTitle.innerHTML = httpRequest.response.Title;
+
+    const movieYear = document.getElementById("movie_year");
+    movieYear.innerHTML = httpRequest.response.Year;
+
+    const movieRating = document.getElementById("rating");
+    movieRating.innerHTML = httpRequest.response.imdbRating;
+
+    const moviePloat = document.getElementById("plot");
+    moviePloat.innerHTML = httpRequest.response.Plot;
   };
 }
