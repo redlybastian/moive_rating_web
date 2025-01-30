27a24c2ae15ce48b410ea176d95f78ee8ae44d7f
redlybastian <red233786@gmail.com>
2025-01-30 13:37:15 +0530

    send request to api

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function search() {
   const userInput = document.getElementById("search_input");
-  const searchButton = document.getElementById("search_button");
+  const movieName = userInput.value;
+  const url = "http://www.omdbapi.com/?i=tt3896198&apikey=235792ba" + movieName;
+  const httpRequest = new XMLHttpRequest();
+
+  httpRequest.open("GET", url);
+  httpRequest.responseType = "json";
+  httpRequest.send();
+
+  httpRequest.onload = function () {
+    console.log(httpRequest.response);
+    const posterImage = document.getElementById("moviePoster");
+    posterImage.src = httpRequest.response.Poster;
+    const movieTitle = document.getElementById("movie_title");
+    movieTitle.innerHTML = httpRequest.response.Title;
+  };
 }
